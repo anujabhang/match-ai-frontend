5e22fee8b975f3ea57b58f9886381766fdae4ee3
anujabhang <abhang.apm@gmail.com>
2024-09-04 12:38:45 +0530

    Initialize React and TailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+
+  //npm install -D stores dependencies into Devdependencies
+}
+
+export default App

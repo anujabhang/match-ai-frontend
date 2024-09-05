47e8654161e39d0ce752fbc12cddbfa5218bcb46
anujabhang <abhang.apm@gmail.com>
2024-09-06 02:26:52 +0530

    Fetch matches and random profiles from API

--- src/components/MatchesList.jsx
+++ src/components/MatchesList.jsx
@@ -1,51 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ChatScreen } from "./ChatScreen";
 
 export const MatchesList = () => {
+  const [matches, setMatches] = useState([]);
+  useEffect(()=>{
+    const fetchMatches = async () =>{
+      const response = await fetch("http://localhost:8081/matches");
+      const data = await response.json();
+      // console.log("data: ", data);
+
+      setMatches(data);
+      
+
+    }
+    fetchMatches();
+  }, [])
+
+  // useEffect(() => {
+  //   console.log("Matches state updated: ", matches);
+  //   // console.log("Matches state id: ", matches[0].id);
+  // }, [matches]);
   return (
     <div className="max-w-screen-md mx-60 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 mx-2">Matches</h2>
       <div className="mx-2 my-2">
         <ul>
-          {[
-            {
-              id: "a4e6d728-17e2-48ca-ba4c-6b980dee7001",
-              firstName: "Maya",
-              lastName: "Smith",
-              age: 25,
-              ethnicity: "Black",
-              gender: "FEMALE",
-              bio: "Creative soul with a love for music and art. Enjoys outdoor activities and spontaneous road trips. Looking for someone to share life\u0027s beautiful moments.",
-              imageUrl:
-                "http://localhost:8081/images/a4e6d728-17e2-48ca-ba4c-6b980dee7001.jpg",
-              myersBriggsPersonalityType: "INFP",
-            },
-            {
-              id: "f8cb8344-862e-435d-b58a-15e0dcc87ac3",
-              firstName: "Sophia",
-              lastName: "Lee",
-              age: 28,
-              ethnicity: "Asian",
-              gender: "FEMALE",
-              bio: "Tech enthusiast and foodie. I enjoy coding by day and exploring new cuisines by night. Let\u0027s create something amazing together!",
-              imageUrl:
-                "http://localhost:8081/images/f8cb8344-862e-435d-b58a-15e0dcc87ac3.jpg",
-              myersBriggsPersonalityType: "INTJ",
-            }
-          ].map((match) => {
+          {matches.map((match) => {
             return (
-                <Link to="/chats">
-              <li key={match.id} className="hover:bg-slate-200">
+                <Link key={match.id} to="/chats">
+              <li  className="hover:bg-slate-200">
                 <button className="flex p-4">
                   <img
-                    src={match.imageUrl}
+                    src={`http://localhost:8081/images/${match.profile.imageUrl}`}
                     alt=""
                     className="w-16 h-16 rounded-full"
                   />
 
                   <h1 className="text-xl font-bold mx-2 ">
-                    {match.firstName} {match.lastName}
+                    {match.profile.firstName} {match.profile.lastName}
                   </h1>
                 </button>
               </li>
--- src/components/ProfileSelector.jsx
+++ src/components/ProfileSelector.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { X, Heart } from "lucide-react";
 
 
 
 export const ProfileSelector = () => {
+  const [profile, setProfile] = useState({});
+  const [clicked, setClicked] = useState(false);
+
+  const handleLeftSwipe = ()=>{
+    clicked?setClicked(false):setClicked(true);
+  }
+
+  const handleRightSwipe = ()=>{
+    clicked?setClicked(false):setClicked(true);
+  }
+  useEffect(()=>{
+    const fetchProfile = async () =>{
+      const response = await fetch("http://localhost:8081/profile/random");
+      const data = await response.json();
+      // console.log("data: ", data);
+
+      setProfile(data);
+      
+
+    }
+    fetchProfile();
+  }, [clicked])
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="overflow-hidden bg-white shadow-lg rounded-lg w-full max-w-md">
         <div className="relative">
           <img
-            src="http://localhost:8081/images/0b1273d4-ab2f-4edd-858b-6f1ff1071fb9.jpg"
+            src={`http://localhost:8081/images/${profile.imageUrl}`}
             alt=""
             className="rounded-lg"
           />
           <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-t from-black">
-            <h2 className="text-3xl font-bold">Anuj Abhang</h2>
+            <h2 className="text-3xl font-bold">{profile.firstName} {profile.lastName}</h2>
           </div>
         </div>
         <div className="p-4">
           <div className="text-gray-600 mb-4">
-            I am a Software Engineer
+            {profile.bio}
           </div>
         </div>
       <div className="flex justify-center space-x-6">
-        <button className="bg-red-500 rounded-full text-white p-2 hover:bg-red-700 my-2"><X size={24}/></button>
-        <button className="bg-green-500 rounded-full text-white p-2 hover:bg-green-700 my-2"><Heart size={24}/></button>
+        <button onClick={handleLeftSwipe} className="bg-red-500 rounded-full text-white p-2 hover:bg-red-700 my-2"><X size={24}/></button>
+        <button onClick={handleRightSwipe} className="bg-green-500 rounded-full text-white p-2 hover:bg-green-700 my-2"><Heart size={24}/></button>
       </div>
       </div>
     </div>
